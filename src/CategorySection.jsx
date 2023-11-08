7410856b2bef232f90a652c4049e90960174464f
Thien Pham <s2201227@edu.bc.fi>
2023-11-08 23:41:16 +0200

    created component CategorySection and its own css file, added the component into the App.jsx

---
+++ src/CategorySection.jsx
@@ -0,0 +1,51 @@
+import './CategorySection.css'
+import sampleIcon from './assets/nightlifeIcon.svg'
+
+function CategorySection() {
+    return (
+        <div className='iconCategoryWrapper'>
+            <div className='iconCategoryCard'>
+                <div className='iconCategoryCardImageWrapper'>
+                    <img src={sampleIcon} alt='icon' />
+                </div>
+                <div className='iconCategoryCardTitle'>
+                    Nightlife
+                </div>
+            </div>
+            <div className='iconCategoryCard'>
+                <div className='iconCategoryCardImageWrapper'>
+                    <img src={sampleIcon} alt='icon' />
+                </div>
+                <div className='iconCategoryCardTitle'>
+                    Nightlife
+                </div>
+            </div>
+            <div className='iconCategoryCard'>
+                <div className='iconCategoryCardImageWrapper'>
+                    <img src={sampleIcon} alt='icon' />
+                </div>
+                <div className='iconCategoryCardTitle'>
+                    Nightlife
+                </div>
+            </div>
+            <div className='iconCategoryCard'>
+                <div className='iconCategoryCardImageWrapper'>
+                    <img src={sampleIcon} alt='icon' />
+                </div>
+                <div className='iconCategoryCardTitle'>
+                    Nightlife
+                </div>
+            </div>
+            <div className='iconCategoryCard'>
+                <div className='iconCategoryCardImageWrapper'>
+                    <img src={sampleIcon} alt='icon' />
+                </div>
+                <div className='iconCategoryCardTitle'>
+                    Nightlife
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CategorySection; 
