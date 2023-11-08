c3cf37247bfc6955813e9aa1839acd7c5b55cad7
johntrehearn <johntrehearnbc@gmail.com>
2023-11-08 19:58:11 +0200

    Footer.jsx added

---
+++ src/assets/footer.jsx
@@ -0,0 +1,16 @@
+import './footer.css'
+
+const Footer = () => {
+    return <div>
+        <div className='footer'>
+
+        <h1>&copy Team 3</h1>
+      
+        </div>
+    </div>
+    
+    
+
+};
+
+export default Footer;
