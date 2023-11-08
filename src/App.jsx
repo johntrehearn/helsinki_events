f4e547b02fcd1837391a1cb66a2b66aab879a097
johntrehearn <johntrehearnbc@gmail.com>
2023-11-08 19:54:58 +0200

    Header added & mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './Header'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <Header/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
