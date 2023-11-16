9382e7f690698ec81f7a1de35de6dbfd4776e436
Thien Pham <s2201227@edu.bc.fi>
2023-11-16 13:40:03 +0200

    map data to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,18 @@
 import '../styles/card.css';
 import sampleImg from '../assets/coffee.jpg';
 
-function Card({events}) {
+function Card({name, date, hour, area}) {
+
     return (
         <div className="eventCard">
             <img src={sampleImg} alt="sample image" className='eventCardImg'></img>
             <section className="eventCardDetails">
-                <h2>{events[0].name}</h2>
+                <h2>{name}</h2>
                 <p>
-                    <span>{events[0].date}</span>
-                    <span>{events[0].hour}</span>
+                    <span>{date}</span>
+                    <span>{hour}</span>
                 </p>
-                <p>{events[0].area}</p>
+                <p>{area}</p>
             </section>
         </div>
     )
--- src/components/CardsBucket.jsx
+++ src/components/CardsBucket.jsx
@@ -2,11 +2,18 @@ import Card from "./Card"
 import '../styles/cardsBucket.css'
 
 function CardsBucket({events}) {
+
+    
+
+
     return (
         <div className="eventCards">
         <h2>EVENTS</h2>
         <div className="eventCardsBucket">
-            <div><Card events={events}/></div>
+            <div>
+            {events.map((event)=> (
+                <Card key={event.id} name={event.name} date={event.date} hour={event.hour} area={event.area}/>
+)) }</div>
         </div>
         </div>
     )
