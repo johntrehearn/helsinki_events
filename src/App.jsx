7b27f2141d7a416e898f7db4186406098baa9741
Thien Pham <s2201227@edu.bc.fi>
2023-11-16 12:26:50 +0200

    add hook useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import Header from './Header'
 import Footer from './footer'
 import Banner from './Banner'
 import CategorySection from './CategorySection'
+import useFetch from './useFetch'
 
 function App() {
+  const event = useFetch("https://api.hel.fi/linkedevents/v1/event/");
+
+  console.log(event.data);
 
   return (
     <>
