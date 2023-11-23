298be1a629259b93ce8e016d555f0ecbd15498f5
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-11-23 12:33:26 +0200

    add isloading, iserror

---
+++ src/components/useFetch.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        // fetch categorizes errors 400 and 500 as a success
+        // .ok categorizes only 200 as a succes
+        if (!response.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+        const data = await response.json();
+        setData(data.data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+};
+
+export default useFetch;
