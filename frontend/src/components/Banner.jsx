a27d6fdf9dd0f5c970349c87e917646cc61d63a4
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-20 18:13:27 +0200

    move search from banner to header and make it fixed

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -9,25 +9,11 @@ const Banner = ({ onchange /* , updateURL */ }) => {
         <img src="../event-bg.jpg" alt="audience" />
       </div>
 
-      <div className="inputs">
+      {/* <div className="inputs">
         <div>
           <Search onchange={onchange} />
         </div>
-        {/*  <button
-          onClick={() =>
-            updateURL("https://api.hel.fi/linkedevents/v1/event/?is_free=true")
-          }
-        >
-          FREE
-        </button>
-        <button
-          onClick={() =>
-            updateURL("https://api.hel.fi/linkedevents/v1/event/?language=en")
-          }
-        >
-          EVENTS IN ENGLISH
-        </button> */}
-      </div>
+      </div> */}
     </div>
   );
 };
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,27 +1,35 @@
-import '../styles/header.css'
+import "../styles/header.css";
+import Search from "./Search";
 
 const Header = () => {
-    return <div>
-        <div className='header'>
-
+  return (
+    <div>
+      <div className="header">
         <h1>Helsinki Events</h1>
-        <nav>
-            {/*  Button for mobile menu */}
-            <button className='mobile'>
-                <span className='material-symbols-outlined'>menu</span>
-                {/* click={() => mobile_menu()} */}
-            </button>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about_the_project">About the project</a></li>
-                <li><a href="contact_us">Contact Us</a></li>
-            </ul>
-        </nav>
+        <div className="inputs">
+          <Search onchange={onchange} />
         </div>
+        <nav>
+          {/*  Button for mobile menu */}
+          <button className="mobile">
+            <span className="material-symbols-outlined">menu</span>
+            {/* click={() => mobile_menu()} */}
+          </button>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about_the_project">About the project</a>
+            </li>
+            <li>
+              <a href="contact_us">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-    
-    
-
+  );
 };
 
 export default Header;
